0338cad87e6686f97aa37cd8bd391ecdc6d5549f
Dragon708 <jorgegmdgonzalez@gmail.com>
2023-01-18 23:01:54 -0500

    Listooo

--- main.js
+++ main.js
@@ -13,25 +13,15 @@
         var sumaparcialDeReferencia = 1
         
         for (let index = 3; index <= longitudLadoMatriz; index += 2 ) {
-            console.log( `variabel ${sumaparcialDeReferencia}`)
             var lllll = sumaparcialDeReferencia
             var valorFinalVuelta = index * index;
                var Diagonal1 = valorFinalVuelta - (index-1)
                var Diagonal2 = Diagonal1 - (index-1)
                var Diagonal3 = Diagonal2 - (index-1)
-
                const sumaDiagonal = valorFinalVuelta + Diagonal1 +Diagonal2 + Diagonal3 
-               console.log(`suma ${sumaDiagonal} `);
-
                sumaparcialDeReferencia = lllll + sumaDiagonal 
-                console.log(sumaparcialDeReferencia)
-
         }
-
-        alert(sumaparcialDeReferencia);
+        alert(` Resultado: ${sumaparcialDeReferencia}`);
         sumaparcialDeReferencia = 1
-
     }
-
-
 }())
